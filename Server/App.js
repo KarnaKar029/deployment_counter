ef6907c50556dd7008cd63bf2bf7091b6a9829b3
KarnaKar@029 <karnubairy@gmail.com>
2024-04-30 13:07:54 +0530

    second

--- Server/App.js
+++ Server/App.js
@@ -92,6 +92,24 @@ app.post('/api/mycounter/decrement', async (req, res) => {
     }
 });
 
+
+
+//-----------------deployment---------------------
+const __dirname1=path.resolve();
+if(process.env.NODE_ENV==="production"){
+        app.use(express.static(path.join(__dirname1,'/frontend/build')))
+        app.get("*",(req,res)=>{
+                res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
+            })
+}
+else{
+    app.get("/",(req,res)=>{
+        res.send("API is running successfully..");
+    })
+}
+
+//-----------------deployment---------------------
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
